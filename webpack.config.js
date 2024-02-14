5f6beb161eb91088bbbe53b018f04a219e1558be
Matt <mattfields+dev@pm.me>
2024-02-15 09:49:30 +1000

    Revert "add webpack; not working"
    This reverts commit 190eac3b04faed3f71dec0959d2a8cf15b4ac0eb.

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  entry: "./src/js/search.js",
-  output: {
-    filename: "search.js",
-    path: path.resolve(__dirname, path.join("dist", "js")),
-  },
-};
