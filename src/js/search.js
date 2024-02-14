2eab83c88cf77c2415889677aac05f558ea9ce75
Matt <mattfields+dev@pm.me>
2024-02-14 11:28:28 +1000

    cleaning up js

--- src/js/search.js
+++ src/js/search.js
@@ -1,24 +1,45 @@
-const DEFAULT_SEARCH_URL = 'https:///www.duckduckgo.com/?q=';
+const DEFAULT_SEARCH_URL = "https:///www.duckduckgo.com/?q=";
 
 // handler function for the search form
 function search() {
-  const el = document.querySelector("#input-search")
-  const val = el.value;
-
-  // Join the search query string by '+' characters
-  const asQueryString = val && val.split(" ").map((x) => x.trim()).join("+") || "";
-  if (!asQueryString.length) {
-    return
+  // Join the search form content by '+' characters
+  const q = toQueryString(getFormContent());
+  if (!q.length) {
+    return;
   }
 
   // Open the search url
-  location.href = DEFAULT_SEARCH_URL + asQueryString;
+  location.href = DEFAULT_SEARCH_URL + q;
+}
+
+// Join content string with "+"
+const toQueryString = (val) => {
+  if (val) {
+    return val
+      .split(" ")
+      .map((x) => x.trim())
+      .join("+");
+  }
+  return "";
+};
+
+// Get the content of the search input field
+function getFormContent() {
+  const el = document.querySelector("#input-search");
+  return el?.value || "";
+}
+
+// Log the input form content
+function debugLogInput() {
+  const val = document.querySelector("#input-search").value;
+  console.log("=== debug: value:", val);
 }
 
 // Add listeners for search form
 function addListeners() {
-  document.querySelector('#search-form').addEventListener('submit', (event) => {
+  document.querySelector("#search-form").addEventListener("submit", (event) => {
     event.preventDefault();
-    search();
+    debugLogInput();
+    // search();
   });
 }
