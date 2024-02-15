a90f3bba32bb2fec4b02beb5a78d4a3990d279db
Matt <mattfields+dev@pm.me>
2024-02-15 13:47:06 +1000

    fix build script

--- build.sh
+++ build.sh
@@ -6,21 +6,27 @@ set -e
 source .env
 source ./script/util.sh
 
+CSS_OUTFILE="${CSS_BUILD_DIR}/style.css"
+
 # Confirm required commands exist
 function sanityChecks() {
   executableExists "npx"
   fileExists .env
+  fileExists ./src/index.html
   notEmpty "${CSS_BUILD_DIR}"
   fileExists "${CSS_OUTFILE}"
 }
 
 # Run sanity checks and build
 function build() {
-  CSS_OUTFILE="${CSS_BUILD_DIR}/style.css"
+  # Copy the HTML stuff
+  cp -f ./src/index.html ./dist/index.html
+
+  # Build the CSS to /css/style.css
   echo -e "\nBuilding CSS to -> ${CSS_OUTFILE}"
   sanityChecks
 
-  # hacky way to check for --watch flag
+  # Hacky way to check for --watch flag
   if [ "${ARGS[0]}" == '--watch' ]; then
     echo "Watching for changes... Ctrl+C to exit"
   fi
