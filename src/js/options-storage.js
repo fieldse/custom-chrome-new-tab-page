594dcfb2f6dacff86edfbceda13d92550af57fdb
Matt <mattfields+dev@pm.me>
2024-02-22 11:50:20 +1000

    add func update placeholder text

--- src/js/options-storage.js
+++ src/js/options-storage.js
@@ -2,28 +2,46 @@
 
 // set the title of the homepage from form value
 function setTitle() {
-  const title = document.getElementById("titleInput")?.value || "";
+  const title = getTitle();
   console.log(`=== debug: set title: `, title);
-  localStorage.setItem("title", title);
+
+  // Store to local storage
+  storeTitle(title);
+
+  // Replace the title on the page
+  replacePageTitle(title);
 }
 
+// Replace the main title of the page
+const replacePageTitle = (title) => {
+  document.getElementById("page-title").innerHTML = title;
+};
+
+// Store title to localstorage
+const storeTitle = (title) => localStorage.setItem("title", title);
+
 // get the homepage title from local storage
-function getTitle() {
-  const title = localStorage.getItem("title");
-  console.log(`=== debug: got title: `, title);
-}
+const getTitle = () => localStorage.getItem("title") || "";
 
 // toggle visibility of the title editor form
 function toggleFormVisible() {
-  const el = document.getElementById("title-editor-form");
-  el.classList.toggle("hidden");
+  document.getElementById("title-editor-form")?.classList.toggle("hidden");
 }
-// set the listener on the button
-function setButtonListeners() {
-  // Add editor button onclick handler
-  document.getElementById("title-editor-submit").onclick = setTitle;
 
-  // Add toggle visibility button handler
+// Set the input form text
+const setInputFormText = (title) =>
+  (document.getElementById("titleInput").value = title);
+
+// Set button actions for editor form submit, toggle visibility
+function setButtonListeners() {
+  const title = getTitle() || "Example Placeholder Title";
+
+  // Set placeholder values
+  setInputFormText(title);
+  replacePageTitle(title);
+
+  // Add button listeners
+  document.getElementById("title-editor-submit").onclick = setTitle;
   document.getElementById("btn-toggle-editor").onclick = toggleFormVisible;
 }
 
