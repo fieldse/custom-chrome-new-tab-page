42267b2c97788c4afd17f634b3d19adcc85f3d05
Matt <mattfields+dev@pm.me>
2024-02-22 12:01:14 +1000

    working update & store page title from localstorage

--- src/js/options-storage.js
+++ src/js/options-storage.js
@@ -1,15 +1,22 @@
 // Functions for setting and getting user preferences from localstorage
 
 // set the title of the homepage from form value
-function setTitle() {
-  const title = getTitle();
-  console.log(`=== debug: set title: `, title);
+function setAndStoreTitle() {
+  const title = document.getElementById("titleInput").value;
+
+  if (!title) {
+    console.log("title empty");
+    return;
+  }
 
   // Store to local storage
   storeTitle(title);
 
   // Replace the title on the page
   replacePageTitle(title);
+
+  // Hide the editor form
+  hideForm();
 }
 
 // Replace the main title of the page
@@ -23,6 +30,10 @@ const storeTitle = (title) => localStorage.setItem("title", title);
 // get the homepage title from local storage
 const getTitle = () => localStorage.getItem("title") || "";
 
+// Hide the title editor form
+const hideForm = () =>
+  document.getElementById("title-editor-form")?.classList.add("hidden");
+
 // toggle visibility of the title editor form
 function toggleFormVisible() {
   document.getElementById("title-editor-form")?.classList.toggle("hidden");
@@ -34,14 +45,14 @@ const setInputFormText = (title) =>
 
 // Set button actions for editor form submit, toggle visibility
 function setButtonListeners() {
-  const title = getTitle() || "Example Placeholder Title";
+  const title = getTitle() || "Welcome to your Example Page";
 
   // Set placeholder values
   setInputFormText(title);
   replacePageTitle(title);
 
   // Add button listeners
-  document.getElementById("title-editor-submit").onclick = setTitle;
+  document.getElementById("title-editor-submit").onclick = setAndStoreTitle;
   document.getElementById("btn-toggle-editor").onclick = toggleFormVisible;
 }
 
