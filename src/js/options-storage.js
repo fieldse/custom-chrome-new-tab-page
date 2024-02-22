db3eb681ed74db7c3474e729255e7b1cf2212ca7
Matt <mattfields+dev@pm.me>
2024-02-22 11:33:04 +1000

    add toggle visible for editor form

--- src/js/options-storage.js
+++ src/js/options-storage.js
@@ -1,26 +1,30 @@
 // Functions for setting and getting user preferences from localstorage
 
-// set the title of the homepage
-function setTitle(title) {
+// set the title of the homepage from form value
+function setTitle() {
+  const title = document.getElementById("titleInput")?.value || "";
+  console.log(`=== debug: set title: `, title);
   localStorage.setItem("title", title);
 }
 
-// get the homepage title
-function getTitle() {}
-
-// get page title from input
-function getTitleInput() {
-  return document.getElementById("titleInput")?.value;
+// get the homepage title from local storage
+function getTitle() {
+  const title = localStorage.getItem("title");
+  console.log(`=== debug: got title: `, title);
 }
 
+// toggle visibility of the title editor form
+function toggleFormVisible() {
+  const el = document.getElementById("title-editor-form");
+  el.classList.toggle("hidden");
+}
 // set the listener on the button
-function setButtonListener() {
-  const btn = document.getElementById("logTitleButton");
-  console.log("found button", btn);
-  btn.onclick = () => {
-    const val = getTitleInput();
-    console.log(`value: ${val}`);
-  };
+function setButtonListeners() {
+  // Add editor button onclick handler
+  document.getElementById("title-editor-submit").onclick = setTitle;
+
+  // Add toggle visibility button handler
+  document.getElementById("btn-toggle-editor").onclick = toggleFormVisible;
 }
 
-setButtonListener();
+setButtonListeners();
