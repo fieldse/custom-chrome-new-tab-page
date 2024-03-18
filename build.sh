9ab51d5e985109807256375ee3e24bf3b90830a2
Matt <mattfields+dev@pm.me>
2024-03-18 13:36:49 +1000

    clean up build script

--- build.sh
+++ build.sh
@@ -3,36 +3,38 @@
 ARGS=($@)
 
 set -e
-source .env
+# source .env
 source ./script/util.sh
 
-CSS_OUTFILE="${CSS_BUILD_DIR}/style.css"
+# Build dirs
+BUILD_DIR=./dist
+CSS_OUTFILE="${BUILD_DIR}/css/style.css"
 
-# Confirm required commands exist
+# Confirm required source files and commands exist
 function sanityChecks() {
-  executableExists "npx"
-  fileExists .env
-  fileExists ./src/index.html
-  notEmpty "${CSS_BUILD_DIR}"
-  fileExists "${CSS_OUTFILE}"
+  executableExists "npx"        # Ensure we have npm/npx command
+  fileExists ./src/index.html   # HTML base templae
+  fileExists ./src/css/main.css # CSS entrypoint
 }
 
 # Run sanity checks and build
 function build() {
-  # Copy the HTML stuff
-  echo "[+] copying index.html -> ${HTML_BUILD_DIR}"
-  cp -f ./src/index.html "${HTML_BUILD_DIR}/"
+  sanityChecks
 
-  # Copy the JS stuff
-  echo "[+] copying /src/cs -> ${JS_BUILD_DIR}"
-  mkdir -p "${JS_BUILD_DIR}"
-  cp -rf ./src/js/* "${JS_BUILD_DIR}/"
+  # Clean the dist directory
+  echo "[+] cleaning /dist"
+  rm -rf "${BUILD_DIR}/*"
+  mkdir -p "${BUILD_DIR}"
+
+  # Copy all the static source files
+  echo "[+] copying static files"
+  cp -rf ./src "${BUILD_DIR}/"
 
   # Build the CSS to /css/style.css
   echo "[+] building CSS to -> ${CSS_OUTFILE}"
-  sanityChecks
 
   # Hacky way to check for --watch flag
+  # TOOD -- add proper command-line flags
   if [ "${ARGS[0]}" == '--watch' ]; then
     echo "[+] watching for changes... Ctrl+C to exit"
   fi
